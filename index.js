20d7446b84edb57f5f98adeee5eeabbdf5fdf856
watsrunner <watsrunner@protonmail.com>
2022-02-21 12:08:30 -0800

    starting repo for learn-webxr-course

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log('listening at ' + port));
+app.use(express.static('./'));
+
+// app.get('/.well-known/acme-challenge/:content', function(req, res) {
+//     res.send('3tPYdfLex9VgznoTrsSdT3iDhMMBPwlOgHtzGtLqzSM.k9l5F51z8IaUgTLz7bZd-unw6t2WWsF9-bygm0ZVkvA')
+//   })
+
