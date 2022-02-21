0b36d296f8af0bfb13f3421e807023efe6cd8dc1
watsrunner <watsrunner@protonmail.com>
2022-02-21 12:54:19 -0800

    lecture 2.2

--- Resources for Learn-WebXR Udemy Course/start/lecture2_2/app.js
+++ Resources for Learn-WebXR Udemy Course/start/lecture2_2/app.js
@@ -6,7 +6,15 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
-        
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth, window.innerHeight, 0.1, 100);
+		this.camera.position.set (0, 0, 4);
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0xaaaaaa);
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
+		this.renderer.setAnimationLoop(this.render.bind(this));
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
@@ -14,8 +22,8 @@ class App{
         
     }
     
-	render( ) {  
-        
+	render( ) {
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
